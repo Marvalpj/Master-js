e2614a1f52e867397046f19ad2323779bf8533fa
Pedro jose <marvalpj28@gmail.com>
2020-05-04 14:01:06 -0400

    practica bom terminada

--- bom/ejercicios/script.js
+++ bom/ejercicios/script.js
@@ -6,15 +6,45 @@ window.addEventListener('load',function(){
     var form = document.querySelector("#form")
     var boxDashed = document.querySelector(".dashed")
     boxDashed.style.display = "none"
+    
     form.addEventListener("submit",function(){
         
         var name = document.querySelector("#name").value
         var lastName = document.querySelector("#lastName").value
-        var age = document.querySelector("#age").value
-
-        var dataUser = [name , lastName , age ]
+        var age = parseInt(document.querySelector("#age").value)
         
+        if( name.trim() == null || name.trim().length == 0 ){
+            alert("el nombre no es valido")
+            document.querySelector("#error_name").innerHTML = "El nombre no es valido"
+            return false
+        }else{
+            document.querySelector("#error_name").style.display = "none"
+            // if (document.querySelector("#error_name").textContent.length > 0){
+            //     document.querySelector("#error_name").textContent = ""
+            // }
+        }
+        
+        if( lastName.trim() == null || lastName.trim().length == 0 ){
+            alert("el apellido no es valido")
+            return false
+        }
+        
+        if( age == null || age <= 0 || isNaN(age) ){
+            alert("La edad no es valida")
+            return false
+        }
+
+
         boxDashed.style.display = "block"
+        
+        //var dataUser = [name , lastName , age ]
+          //agregando informacion a html
+        // for( let data in dataUser){
+        //     var parrafo = document.createElement("p")
+        //     parrafo.append(dataUser[data])
+        //     boxDashed.append(parrafo)
+        // }
+      
 
         var p_name = document.querySelector("#p-name span")
         var p_lastName = document.querySelector("#p-lastName span")
@@ -24,12 +54,7 @@ window.addEventListener('load',function(){
         p_lastName.innerHTML = lastName
         p_age.innerHTML = age
 
-        //agregando informacion a html
-        // for( let data in dataUser){
-        //     var parrafo = document.createElement("p")
-        //     parrafo.append(dataUser[data])
-        //     boxDashed.append(parrafo)
-        // }
+      
 
     })
 
