648469687bb052b585404eff3ec63448f7728ab4
Pedro jose <marvalpj28@gmail.com>
2020-05-15 13:21:27 -0400

    continuacion fetch y js asincrono

--- fetch/index.js
+++ fetch/index.js
@@ -6,27 +6,79 @@ var usuarios = []
 
 // var prueba
 
-var box = document.querySelector("#users")
+var boxUsers = document.querySelector("#users")
+var boxUser = document.querySelector("#user")
 
-fetch('https://reqres.in/api/users?page=2')
+    getUsuarios()
     .then( data => data.json() )
     .then(  users => {
-        usuarios  = users.data
-        console.log( usuarios )
+        listarUsuarios(users.data)
 
-        usuarios.map( ( user , i ) => {
-            let nombre =  document.createElement("h2")
-            nombre.innerHTML = i + "." + user.first_name + " " + user.last_name
-            box.appendChild(nombre)
-        })
+        return getUsuario()
     })
+    .then( data => data.json())
+    .then( user => {
+        loadDataUser(user.data)
 
-    //asi tambien funciona
-// fetch('https://jsonplaceholder.typicode.com/users')
-//     .then( function( data ) {
-//         return data.json()
-//     } )
-//     .then(  function( data ){
-//         prueba = data
-//         console.log( prueba )
-//     })
+        return getInfo()
+    })
+    .then(data => {
+        console.log( 'data: '+ " "+ data)
+    })
+    
+
+
+function getUsuarios(){
+    return fetch('https://reqres.in/api/users?page=2')
+} 
+
+function getUsuario(){
+    return fetch('https://reqres.in/api/users/2')
+}
+  
+
+function getInfo(){
+    var profesor = {
+        nombre: "victor",
+        apellido: "robles",
+        url: "https://www.victorroblesweb.es"
+    }
+    return new Promise( (resolve , reject) => {
+        var profesor_string = ''
+        setTimeout(function(){
+            profesor_string = JSON.stringify(profesor)
+        }, 5000)
+        
+
+        if( typeof profesor_string  != 'string'){
+            return reject('error')
+        }    
+        return resolve(profesor_string)
+    }) 
+}
+
+function listarUsuarios( usuarios ){
+    usuarios.map( ( user , i ) => {
+        let nombre =  document.createElement("h2")
+        nombre.innerHTML = i + "." + user.first_name + " " + user.last_name
+        document.querySelector("#users .loading").style.display = "none"
+        boxUsers.appendChild(nombre)
+    })
+}
+
+function loadDataUser( usuario){
+    let nombre = document.createElement("h2")
+    let avatar = document.createElement("img")
+
+    nombre.innerHTML = usuario.first_name + " " + usuario.last_name
+    
+    avatar.src = usuario.avatar
+    avatar.width = '100'
+    
+    boxUser.appendChild(nombre)
+    boxUser.appendChild(avatar)
+
+    document.querySelector("#user .loading").style.display = "none"
+}
+
+
